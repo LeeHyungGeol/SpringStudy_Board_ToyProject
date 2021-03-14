34db93ed1dd0b0b7fef36ffc2d573072f3b0b9e3
LeeHyungGeol <rjf1138@naver.com>
2021-03-14 18:09:56 +0900

    'domain-boardVO'

---
+++ src/main/java/com/board_toy_project/domain/BoardVO.java
@@ -0,0 +1,31 @@
+package com.board_toy_project.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
+//JPA 엔티티 매핑
+@Entity
+public class BoardVO {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) //DB에 값을 넣으면, DB가 자동으로 Id를 생성해주는 것을 "Identity 전략" 이라고 한다.
+    private int id;
+    private String title;
+    private String content;
+    private String writer;
+    private int password;
+    private Timestamp timestamp;
+
+    public BoardVO() {}
+
+    public BoardVO(int id, String title, String content, String writer, int password, Timestamp timestamp) {
+        super();
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+        this.password = password;
+        this.timestamp = timestamp;
+    }
+}
