c8ba4c42fdf6884ca355eb7c0ba150d1189dcd86
LeeHyungGeol <rjf1138@naver.com>
2021-03-14 17:16:18 +0900

    'ThymeleafEngineTest'

---
+++ src/main/java/com/board_toy_project/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.board_toy_project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello-mvc")
+    public String helloMVC(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+}
