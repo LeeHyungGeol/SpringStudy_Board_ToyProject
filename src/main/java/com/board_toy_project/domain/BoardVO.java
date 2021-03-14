9784133618bde6b4b74799bf60e30aa8f46b0b37
LeeHyungGeol <rjf1138@naver.com>
2021-03-14 19:43:02 +0900

    H2 database

--- src/main/java/com/board_toy_project/domain/BoardVO.java
+++ src/main/java/com/board_toy_project/domain/BoardVO.java
@@ -1,5 +1,8 @@
 package com.board_toy_project.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,24 +11,22 @@ import java.sql.Timestamp;
 
 //JPA 엔티티 매핑
 @Entity
+@Getter @Setter
 public class BoardVO {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) //DB에 값을 넣으면, DB가 자동으로 Id를 생성해주는 것을 "Identity 전략" 이라고 한다.
     private int id;
     private String title;
     private String content;
     private String writer;
-    private int password;
     private Timestamp timestamp;
 
     public BoardVO() {}
 
-    public BoardVO(int id, String title, String content, String writer, int password, Timestamp timestamp) {
-        super();
+    public BoardVO(int id, String title, String content, String writer, Timestamp timestamp) {
         this.id = id;
         this.title = title;
         this.content = content;
         this.writer = writer;
-        this.password = password;
         this.timestamp = timestamp;
     }
 }
